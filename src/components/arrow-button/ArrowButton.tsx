3c46039042178adf63a7c23ce1bd1561440ff664
Policarp <tatarkin-evgeniy@mail.ru>
2025-04-07 15:44:29 +0300

    feat: add basic selectors to the articleParamsForm with state updating on font change

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -7,7 +7,11 @@ import styles from './ArrowButton.module.scss';
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+export type TArrowButtonProps = {
+	onClick?: OnClick
+}
+
+export const ArrowButton = ({onClick} : TArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
@@ -15,6 +19,7 @@ export const ArrowButton = () => {
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
 			className={styles.container}
+			onClick={onClick}
 		>
 			<img
 				src={arrow}
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,21 +2,35 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
-import { ArticleStateType } from 'src/constants/articleProps';
+import { ArticleStateType, backgroundColors, contentWidthArr, fontColors, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
+import { Select } from '../select';
+import { useState } from 'react';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
 
 export type TArticleProps = {
 	currentState: ArticleStateType,
 	setter: (state: ArticleStateType) => void;
 }
 
-export const ArticleParamsForm = ({setter, currentState}: TArticleProps) => {
+export type TConfigurationState = ArticleStateType & {
+	opened: boolean
+}
 
+export const ArticleParamsForm = ({setter, currentState: prevState}: TArticleProps) => {
+	const [state, setState] = useState({...prevState, opened: false});
 	return (
 		<>
-			<ArrowButton />
+			<ArrowButton onClick={() => setState(prev => ({ ...prev, opened: !prev.opened }))}/>
 			<aside
-				className={styles.container}>
+				className={`${styles.container} ${state.opened ? styles.container_open : ""}`}>
 				<form className={styles.form}>
+					<Select selected={state.fontFamilyOption} title='Шрифт' options={fontFamilyOptions} onChange={opt => setState({ ...state, fontFamilyOption: opt})}/>
+					<RadioGroup name='kek' title='Размер шрифта' selected={state.fontSizeOption} options={fontSizeOptions}/>
+					<Select selected={state.fontColor} title='Цвет шрифта' options={fontColors}/>
+					<Separator/>
+					<Select selected={state.backgroundColor} title='Цвет фона' options={backgroundColors}/>
+					<Select selected={state.contentWidth} title='Ширина контента' options={contentWidthArr}/>
 					<div className={styles.bottomContainer}>
 						<Button
 							title='Сбросить'
